24837fc8209c3e0c4fa72ee5d15a77873c190298
Hong <henrylei80@yahoo.com>
2019-10-26 17:42:04 -0400

    pushed code into github

---
+++ public/build/precache-manifest.995dd9ce156ccf04d2b762ffe6ed70b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "726636bc2b9b643db81004c3513f3179",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e26601f29ee2b9e40e8c",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "55ad6a2b8a5fb9cb6c5e",
+    "url": "/static/js/2.7f4ef919.chunk.js"
+  },
+  {
+    "revision": "e26601f29ee2b9e40e8c",
+    "url": "/static/js/main.488de0cd.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  },
+  {
+    "revision": "e4eb53816d15621d9b30cb22c20aa763",
+    "url": "/static/media/logo.e4eb5381.jpg"
+  }
+]);
